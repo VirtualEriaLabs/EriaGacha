8c8787f3c8b5e2259cd3af2e69e1f1aca60f2794
AdanJoGoHe <adanjogohe@protonmail.com>
2021-07-17 22:05:18 +0100

    Añadido niveles a los hechizos y arreglos varios

--- src/main/java/net/eriagacha/GachaItem.java
+++ src/main/java/net/eriagacha/GachaItem.java
@@ -1,5 +1,6 @@
 package net.eriagacha;
 
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -7,11 +8,12 @@ import net.minecraft.util.registry.Registry;
 
 public class GachaItem {
 
-  //TODO : Investigar PlayerItemConsumeEvent
-  private static final Item GACHA_COOKIE = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(3));
+  //TODO : Investigar PlayerItemConsumeEvent para que se ejecute el comando al comer
+  public static final Item GACHA_COOKIE = new Item(new Item.Settings()
+      .group(ItemGroup.MISC).food(new FoodComponent.Builder().hunger(1).saturationModifier(1f).snack().alwaysEdible().build()));
 
   public static void init(){
 
-    Registry.register(Registry.ITEM, new Identifier("gacheria", "gacha_cookie"), GACHA_COOKIE);
+    Registry.register(Registry.ITEM, new Identifier("eriagacha", "gacha_cookie"), GACHA_COOKIE);
   }
 }
