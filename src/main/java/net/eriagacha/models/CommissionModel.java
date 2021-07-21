e6b4e6738fba9105a2da91d4aee9e2177ab67951
daiya_ <apalfonso23@gmail.com>
2021-07-21 20:57:29 +0200

    refactor: adds serializble to model classes

--- src/main/java/net/eriagacha/models/CommissionModel.java
+++ src/main/java/net/eriagacha/models/CommissionModel.java
@@ -1,6 +1,8 @@
 package net.eriagacha.models;
 
 
+import java.io.Serial;
+import java.io.Serializable;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
@@ -9,7 +11,11 @@ import lombok.extern.log4j.Log4j2;
 @Data
 @SuperBuilder
 @Log4j2
-public abstract class CommissionModel{
+public class CommissionModel implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 8642948807010652686L;
+
   private String commissionName;
   private String commissionTask;
 }
--- src/main/java/net/eriagacha/models/CommissionModelItem.java
+++ src/main/java/net/eriagacha/models/CommissionModelItem.java
@@ -1,9 +1,11 @@
 package net.eriagacha.models;
 
 
+import java.io.Serial;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 import lombok.extern.log4j.Log4j2;
 
 
@@ -13,6 +15,9 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Jacksonized
 public class CommissionModelItem extends CommissionModel {
+
+  @Serial
+  private static final long serialVersionUID = 8642948801310652686L;
 
   private int itemNeeded;
   private int itemQuantity;
