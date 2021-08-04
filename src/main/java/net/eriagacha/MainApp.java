01d804085d37c30a3d3161785b2eaf1aae7888fe
AdanJoGoHe <adanjogohe@protonmail.com>
2021-08-04 20:36:21 +0100

    fix: method call missing

--- src/main/java/net/eriagacha/MainApp.java
+++ src/main/java/net/eriagacha/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp {
 
   public static void main(String[] args) throws SQLException {
 
-
+    insertSuccessfullTest();
     insertFailedTest();
     insertSuccessfullAndFailedTest();
     /*
