e7e3143e3f8fcaa1767b580a481e754f0e17a054
AdanJoGoHe <adanjogohe@protonmail.com>
2021-08-04 21:44:21 +0100

    feat: Database Insert for gacha items

---
+++ src/main/java/net/eriagacha/repository/GachaRepository.java
@@ -0,0 +1,43 @@
+package net.eriagacha.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import lombok.extern.log4j.Log4j2;
+import net.data.datasource.DataSource;
+import net.eriagacha.models.GachaTelemetryModel;
+
+@Log4j2
+public class GachaRepository {
+
+
+  public void insertGachaTelemetry(GachaTelemetryModel gtm) throws SQLException {
+    String insertString = """
+        INSERT INTO GACHA_TELEMETRY(Reward,User,Date) VALUES (?,?,?)
+        """;
+
+    Connection con = DataSource.getConnection();
+
+    try (PreparedStatement test = con.prepareStatement(insertString)) {
+      con.setAutoCommit(false);
+      test.setString(1, gtm.getRewardObtained());
+      test.setString(2, gtm.getPlayerName());
+      test.setString(3, gtm.getDate());
+      test.execute();
+      con.commit();
+    } catch (SQLException e) {
+      log.error(e.getMessage());
+      if (con != null) {
+        try {
+          log.error("Transaction is being rolled back");
+          con.rollback();
+        } catch (SQLException excep) {
+          log.error(excep.getMessage());
+        }
+      }
+    }
+    finally {
+      con.close();
+    }
+  }
+}
