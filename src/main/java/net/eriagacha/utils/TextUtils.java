24933bca6e5088ccf9e90f820b01085c0a18d75b
AdanJoGoHe <adanjogohe@gmail.com>
2021-08-22 03:56:26 +0100

    refactor: linebreak

--- src/main/java/net/eriagacha/utils/TextUtils.java
+++ src/main/java/net/eriagacha/utils/TextUtils.java
@@ -5,7 +5,6 @@ import net.minecraft.text.TranslatableText;
 public class TextUtils {
 
   public static String translatedTextToString(String keyOfTranslatedText){
-
     return new TranslatableText(keyOfTranslatedText).getString();
   }
 }
