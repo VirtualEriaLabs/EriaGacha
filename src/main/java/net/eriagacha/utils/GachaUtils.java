fa51589d4059f1d919186c611b3751438634c7f4
Adán <adanjogohe@gmail.com>
2021-08-23 10:22:22 +0100

    fix: wrong end item

--- src/main/java/net/eriagacha/utils/GachaUtils.java
+++ src/main/java/net/eriagacha/utils/GachaUtils.java
@@ -2,9 +2,8 @@ package net.eriagacha.utils;
 
 import net.eriagacha.RegisterItems;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 
 public class GachaUtils {
-  public static final ItemStack CHEAP_GACHA_REQUIEREMENT = new ItemStack(Items.DIRT);
+  public static final ItemStack CHEAP_GACHA_REQUIEREMENT = new ItemStack(RegisterItems.ACQUAINT_FATE);
   public static final ItemStack EXPENSIVE_GACHA_REQUIEREMENT = new ItemStack(RegisterItems.INTERTWINED_FATE);
 }
